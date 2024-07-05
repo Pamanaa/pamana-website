ea2065866fa2c8275b2f0c910b312a0b805e75ca
carlmitzchel <carlmitzchel@gmail.com>
2024-07-05 12:27:31 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <h1 className="dongle-header-active">TEST</h1>
+      <h1 className="dongle-header-regular">localhost:3000/sign-up</h1>
+      <h1 className="dongle-header-regular">localhost:3000/sign-in</h1>
+      <h1 className="dongle-header-regular">- No validations yet</h1>
+    </>
+  );
 }
