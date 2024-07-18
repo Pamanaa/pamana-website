9a841c37bb4e43b1ff33091fb30b1d7645578059
Lenci Cruz <kristellenci@gmail.com>
2024-07-18 16:45:33 +0800

    added gallery grid component

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import GalleryComponent from "../../components/Gallery/gallery-grid";
+import Image from "next/image";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <> 
+      <div className="flex justify-center items-center p-6">
+        <h1 className="text-center text-[50px]">
+        <span className="font-ephesis">Explore </span> <span className="font-dongle">and </span> 
+        <span className="font-ephesis">imagine </span> <span className="font-dongle">your perfect getaway at Pamana.</span>
+        </h1>
+      </div>
+      <GalleryComponent />
+    </>
+  );
 };
 
 export default Gallery;
