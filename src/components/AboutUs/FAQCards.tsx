9b366c9301bb1dad1526e64b5206865740c63a0d
Jerome Mangalus <jeromemangalus@gmail.com>
2024-07-30 14:28:05 +0800

    About Us Page # [Title of PR]
    ## Details
    - Created the draft for all the sections of About Us page.
    
    ## Juxtaposition
    
    ## Additional Notes
    
    - Page is not yet responsive
    - Map is not yet integrated
    - No Footer

---
+++ src/components/AboutUs/FAQCards.tsx
@@ -0,0 +1,33 @@
+// components/FAQCard.tsx
+
+import React from "react";
+import styles from "./FAQCards.module.css";
+
+interface FAQCardProps {
+  question: string;
+  answer: string;
+}
+
+const FAQCards: React.FC<FAQCardProps> = ({ question, answer }) => {
+  return (
+    <>
+      <div className="relative">
+        <div className="mx-auto min-h-[450px] max-w-[375px] rounded-3xl border-2 border-[#8DBC83] bg-white p-6 text-center">
+          <div className="absolute -top-8 left-1/2 flex h-20 w-20 -translate-x-1/2 transform items-center justify-center rounded-full bg-[#8DBC83] text-[70px] font-extrabold text-white">
+            <span>?</span>
+          </div>
+          <div className="mt-6">
+            <h3 className="mb-2 mt-4 text-[40px] font-bold text-[#7A4424]">
+              {question}
+            </h3>
+            <p className="text-[30px] leading-normal text-[#77442B]">
+              {answer}
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FAQCards;
