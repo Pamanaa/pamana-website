5b2ecc2c37e2867764759a604d791e39fb2fb193
JoshuaEntrata <kesselentrata@gmail.com>
2025-03-30 22:09:02 +0800

    Minor fix in Gallery

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -43,10 +43,23 @@ const Wedding = ["/images/home/wedding.jpg"];
 // TODO: Apply to Home page
 const Aerial = ["/images/home/aerial.jpg"];
 
+const GalleryImgs = [
+  "/images/gallery/1.png",
+  "/images/gallery/2.jpg",
+  "/images/gallery/3.jpg",
+  "/images/gallery/4.jpg",
+  "/images/gallery/5.jpg",
+  "/images/gallery/6.jpg",
+  "/images/gallery/7.jpg",
+  "/images/gallery/8.jpg",
+  "/images/gallery/9.jpg",
+];
+
 export {
   Aerial,
   DefaultHomeImgs,
   ExclusiveOffers,
+  GalleryImgs,
   PamanaEvents,
   WaveImgs,
   RoomImgs,
