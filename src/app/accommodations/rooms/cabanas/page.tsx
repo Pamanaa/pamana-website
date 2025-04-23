b9bbb8f7c419fbb9fefe78e1667704620cb6588e
Carl Mitzchel Padua <carlmitzchel@gmail.com>
2025-04-23 22:46:13 +0800

    Updated look

--- src/app/accommodations/rooms/cabanas/page.tsx
+++ src/app/accommodations/rooms/cabanas/page.tsx
@@ -1,78 +1,119 @@
-import React from "react";
+"use client";
 import Image from "next/image";
 import { FaBed, FaWifi, FaPerson, FaTv } from "react-icons/fa6";
 
 const Cabanas = () => {
   return (
-    <div className="container mx-auto px-4">
-      <div className="relative mb-8 h-[400px] w-full">
-        <h1 className="absolute inset-0 z-10 flex items-center justify-center text-6xl text-white cormorant-sc-t1-bold">
+    // Incorporate hover or onclick effects
+    <div className="mx-auto max-w-[1440px]">
+      <div className="relative h-[613px] w-full">
+        <h1 className="absolute inset-0 z-10 flex items-center justify-center text-7xl text-white cormorant-sc-t1-bold">
           CABANAS
         </h1>
         <div className="absolute inset-0 z-[5] bg-black/30" />
         <Image
-          src="/images/rooms/cabanas-hero.jpg"
+          src="/images/rooms/cabana.png"
           fill
           className="object-cover"
+          sizes="100vw"
           priority
+          alt="Cabana Room Image"
         />
       </div>
 
-      <div className="mb-12 grid gap-8 md:grid-cols-2">
-        <div className="h-75 flex flex-col items-center justify-center">
-          <h3 className="text-center dongle-h3-light">
-            Prepare your palate, for imaginative organic dishes, mouth-watering
-            local favorites, and delicate pastries at Pamana. Prepare your
-            palate, for imaginative organic dishes, mouth-watering local
-            favorites, and delicate pastries at Pamana.
-          </h3>
-          <div className="mt-8 grid grid-cols-2 gap-9">
-            <div className="flex h-48 w-full items-center justify-center align-middle">
-              <div className="flex flex-col items-center justify-center align-middle">
+      <div className="grid grid-cols-4 gap-0">
+        <div className="relative h-[260px] overflow-hidden">
+          <Image
+            src="/images/rooms/cabana.png"
+            width={360}
+            height={260}
+            className="h-full w-full object-cover"
+            alt="Cabana Detail 1"
+          />
+        </div>
+        <div className="relative h-[260px] overflow-hidden">
+          <Image
+            src="/images/rooms/cabana.png"
+            width={360}
+            height={260}
+            className="h-full w-full object-cover"
+            alt="Cabana Detail 2"
+          />
+        </div>
+        <div className="relative h-[260px] overflow-hidden">
+          <Image
+            src="/images/rooms/cabana.png"
+            width={360}
+            height={260}
+            className="h-full w-full object-cover"
+            alt="Cabana Detail 3"
+          />
+        </div>
+        <div className="relative h-[260px] overflow-hidden">
+          <Image
+            src="/images/rooms/cabana.png"
+            width={360}
+            height={260}
+            className="h-full w-full object-cover"
+            alt="Cabana Detail 4"
+          />
+        </div>
+      </div>
+
+      <div className="px-16 py-12">
+        <div className="grid grid-cols-2 gap-16">
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="max-w-[620px] text-center leading-8 text-brown dongle-h3-light">
+              Prepare your palate, for imaginative organic dishes,
+              mouth-watering local favorites, and delicate pastries at Pamana.
+              Prepare your palate, for imaginative organic dishes,
+              mouth-watering local favorites, and delicate pastries at Pamana.
+            </h3>
+            <div className="mt-12 grid w-full grid-cols-2 gap-x-16 gap-y-8">
+              <div className="flex flex-col items-center text-center text-lightbrown">
                 <FaBed />
                 <h1 className="dongle-h3-light">Queen Bed</h1>
               </div>
-            </div>
-            <div className="flex h-48 w-full items-center justify-center align-middle">
-              <div className="flex flex-col items-center justify-center align-middle">
+              <div className="flex flex-col items-center text-center text-lightbrown">
                 <FaWifi />
                 <h1 className="dongle-h3-light">150 MBPS Speed</h1>
               </div>
-            </div>
-            <div className="flex h-48 w-full items-center justify-center align-middle">
-              <div className="flex flex-col items-center justify-center align-middle">
+              <div className="flex flex-col items-center text-center text-lightbrown">
                 <div className="flex flex-row">
-                  {" "}
                   <FaPerson />
                   <FaPerson />
                 </div>
                 <h1 className="dongle-h3-light">2-person Capacity</h1>
               </div>
-            </div>
-            <div className="flex h-48 w-full items-center justify-center align-middle">
-              <div className="flex flex-col items-center justify-center align-middle">
+              <div className="flex flex-col items-center text-center text-lightbrown">
                 <FaTv />
                 <h1 className="dongle-h3-light">TV with 50 Channels</h1>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-col space-y-12 rounded-xl border border-brown bg-transparent p-8 shadow-md">
-          <h2 className="ephesis-h2">Reserve Room</h2>
-          <hr />
-          <div className="justify-left flex flex-row items-center">
-            <h1 className="dongle-title-regular">₱ 4,350</h1>
-            <h1 className="dongle-h2-light">/night</h1>
-          </div>
-          <div className="dongle-h3-light">
-            Lorem ipsum dolor sit amet. Qui doloribus corrupti aut quia veniam
-            sed internos soluta.
-          </div>
+          <div className="flex flex-col rounded-xl border border-brown bg-transparent p-8 shadow-md">
+            <h2 className="mb-4 text-3xl text-brown ephesis-h2">
+              Reserve Room
+            </h2>
+            <hr className="mb-6 border-brown" />
+            <div className="mb-6 flex flex-col">
+              <div className="mb-2 flex flex-row items-baseline gap-1">
+                <span className="text-5xl text-brown dongle-title-regular">
+                  ₱ 4,350
+                </span>
+                <span className="text-brown dongle-h2-light">/night</span>
+              </div>
+              <div className="text-brown dongle-h3-light">
+                Lorem ipsum dolor sit amet. Qui doloribus corrupti aut quia
+                veniam sed internos soluta.
+              </div>
+            </div>
 
-          <button className="hover:bg-green-700 w-full rounded-lg bg-green px-5 py-2 text-white transition-colors dongle-h3-regular">
-            BOOK NOW
-          </button>
+            <button className="hover:bg-green-700 mt-auto w-full rounded-lg bg-green px-5 py-3 text-white transition-colors dongle-h3-regular">
+              BOOK NOW
+            </button>
+          </div>
         </div>
       </div>
     </div>
